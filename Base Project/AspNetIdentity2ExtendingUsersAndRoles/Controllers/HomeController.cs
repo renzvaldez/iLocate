be76a06c186fcdd1c68d1b95a659f546d6ce2962
renzvaldez <valdezmichaelrenz@yahoo.com>
2017-05-21 17:13:12 +0800

    Initial Commit

---
+++ Base Project/AspNetIdentity2ExtendingUsersAndRoles/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using IdentitySample.Models;
+
+namespace IdentitySample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            
+            ViewBag.Message = User.Identity.GetUserId();
+
+            return View();
+        }
+
+        [Authorize(Roles = "User")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
